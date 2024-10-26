abb4dbe7d54eb7baef9c13f3826c5e9849389788
Varghese Abraham <varnit2010@gmail.com>
2024-10-26 09:43:33 -0400

    Generate dysh application template

---
+++ apps/dysh/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dysh-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'dysh';
+}
